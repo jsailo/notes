1a049a210206efb9ba7df64aa20475fcfddb0399
jsailo <jimmy.sailo@gmail.com>
2023-10-02 17:49:37 +0530

    Change base url to /notes/

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   trailingSlash: false,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/notes/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
