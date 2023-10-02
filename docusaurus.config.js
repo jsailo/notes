4375f2a2e176fab0a2bccc6a0e88c85d1e2cff71
jsailo <jimmy.sailo@gmail.com>
2023-10-02 17:13:42 +0530

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'jsailo', // Usually your GitHub org/user name.
   projectName: 'notes', // Usually your repo name.
-  deploymentBranch: 'gh-pages', // Usually your repo name.
+
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
