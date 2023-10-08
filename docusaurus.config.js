6857c2f3e5ec99543186600a6a5d8e8409e9097f
jsailo <jimmy.sailo@gmail.com>
2023-10-08 12:58:50 +0530

    new soc card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/social-card-01.png',
+      image: 'img/social-card-03.png',
       navbar: {
         title: '',
         logo: {
